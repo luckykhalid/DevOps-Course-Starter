fd7f929f024d4348d9830b6640f1f997a1e3e5d7
Khalid Ashraf <KA102@ntrs.com>
2020-12-29 16:09:31 +0000

    Stretch Goal - Mark an item completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, get_item, save_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,12 +11,17 @@ def index():
     items = get_items()
     return render_template('index.html', items=items)
 
-
 @app.route('/', methods=['POST'])
 def create_item():
     add_item(request.form['title'])    
     return index()
 
+@app.route('/markcomplete/<id>')
+def mark_complete(id):
+    item = get_item(id)
+    item['status'] = 'Completed'
+    save_item(item)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
