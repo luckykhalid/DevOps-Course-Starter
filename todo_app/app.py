4847792ee660d20d47bbb941b099912cb4343b85
Khalid Ashraf <KA102@ntrs.com>
2020-12-29 15:31:20 +0000

    Create new todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +10,12 @@ app.config.from_object(Config)
 def index():
     items = get_items()
     return render_template('index.html', items=items)
+
+
+@app.route('/', methods=['POST'])
+def create_item():
+    add_item(request.form['title'])    
+    return index()
 
 
 if __name__ == '__main__':
