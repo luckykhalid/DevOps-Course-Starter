d2cc66105bb248ddb9aff0cd6afebac1f1a98f77
Khalid Ashraf <KA102@ntrs.com>
2021-02-09 16:16:11 +0000

    added basin MVP functionality

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,41 +2,63 @@ from flask import Flask, render_template, request, redirect, send_from_directory
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item, get_item, save_item, get_current_sort_order, delete_item, set_current_sort_order
 import os
+import requests
+import json
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+headers = {
+    "Accept": "application/json"
+}
+
+#postcode_response = requests.get("https://api.postcodes.io/postcodes/NW51TL")
+
+
+query = {    
+}
+
+postcode_response = requests.request("GET",
+                            "https://api.postcodes.io/postcodes/NW51TL",
+                            headers=headers,
+                            params=query
+                            )
+
+#longitude = postcode_response.json().result.longitude
+#latitude = postcode_response.json().result.latitude
+
+#postcode_response = requests.get("api.postcodes.io/postcodes/NW5 1TL")
+
+
+url = "https://transportapi.com/v3/uk/bus/stop/490015367S/live.json"
+
+
+
+query = {
+    'app_id': '23e33db4',
+    'app_key': '7972b59c792d28b4819f58a1a13e7a62',
+    'group': 'route',
+    'limit': 5,
+    'nextbuses': 'yes'
+}
+
+response = requests.request("GET",
+                            url,
+                            headers=headers,
+                            params=query
+                            )
 
 
 @app.route('/')
 def index():
     items = get_items()
-    return render_template('index.html', items=items, sort=get_current_sort_order())
+    return render_template('index.html', items=items, sort=get_current_sort_order(), response=response, postcode_response=postcode_response)
 
 
 @app.route('/', methods=['POST'])
 def create_item():
     add_item(request.form['title'])
     return index()
-
-
-@app.route('/markcomplete/<id>')
-def mark_complete(id):
-    item = get_item(id)
-    item['status'] = 'Completed'
-    save_item(item)
-    return redirect('/')
-
-
-@app.route('/sortby/<sortby>')
-def sort_by(sortby):
-    set_current_sort_order(sortby)
-    return redirect('/')
-
-
-@app.route('/deleteitem/<id>')
-def remove_item(id):
-    delete_item(id)
-    return redirect('/')
 
 
 @app.route('/favicon.ico')
