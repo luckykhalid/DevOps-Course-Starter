8fc13461d45f2d5db3c84f17cf56b4174ef2fc01
Khalid Ashraf <luckykhalid@gmail.com>
2021-03-17 16:50:35 +0000

    Step 3: Patch the call to the Trello API

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 from todo_app.app import create_app
 import pytest
 from dotenv import find_dotenv, load_dotenv
+from unittest.mock import patch, Mock
+
+
+TEST_BOARD_ID = '60254e45c1345502980ecbf7'
 
 
 @pytest.fixture
@@ -13,3 +17,23 @@ def client():
     # Use the app to create a test_client that can be used in our tests.
     with test_app.test_client() as client:
         yield client
+
+
+def test_index_page(client):
+    response = client.get('/')
+
+
+@patch('requests.get')
+def test_index_page_mocked(mock_get_requests, client):
+    # Replace call to requests.get(url) with our own function
+    mock_get_requests.side_effect = mock_get_lists
+    response = client.get('/')
+
+
+def mock_get_lists(url, params):
+    if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/lists':
+        response = Mock()
+        # sample_trello_lists_response should point to some test response data
+        response.json.return_value = 'sample_trello_lists_response'
+        return response
+    return None
