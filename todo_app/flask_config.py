d3d86769bc56fc2d0245dc0a77aacd441860b7ce
Khalid Ashraf <luckykhalid@gmail.com>
2021-06-15 16:06:21 +0100

    Passing .env to docker run command

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -1,15 +1,11 @@
 import os
-from dotenv import find_dotenv, load_dotenv
+
 
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     def __init__(self) -> None:
-        # Load environment variables from .env file for Prod WSGI web server gunicorn (not required for dev env flask run)
-        file_path = find_dotenv('.env')
-        load_dotenv(file_path, override=True)
-
         """Base configuration variables."""
         SECRET_KEY = os.environ.get('SECRET_KEY')
         if not SECRET_KEY:
