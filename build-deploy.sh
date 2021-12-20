417a05ae99b933b98b01fcc420dc87ee169bf706
Khalid Ashraf <luckykhalid@gmail.com>
2021-12-20 13:59:07 +0000

    azure based website

--- build-deploy.sh
+++ build-deploy.sh
@@ -3,11 +3,6 @@
 # Stop the execution of the script if a command has an error
 set -e
 
-# Build prod Docker image and push to Docker repo
-echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
-docker build --target prod --tag "${DOCKER_USER}"/todo_app:latest --tag "${DOCKER_USER}"/todo_app:"${TRAVIS_COMMIT}" .
-docker push "${DOCKER_USER}"/todo_app
-
 # Push same image to Heroku repo
 docker tag "${DOCKER_USER}"/todo_app registry.heroku.com/"${HEROKU_APP}"/web
 docker login --username=_ --password="${HEROKU_API_KEY}" registry.heroku.com
